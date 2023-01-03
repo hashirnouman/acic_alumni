5f7db2276b35d8608858e9919dcf96a8c033658f
hashirnouman <hashirnouman76@gmail.com>
2023-01-03 10:41:47 +0500

    cors enabled

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 // imports of packages
 const express = require("express");
-const bodyParses = require("body-parser");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 // middlewares
-app.use(bodyParses.json());
-app.use(express.json);
-app.use(cors);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
+app.listen(4300, () => {
+  console.log("listing on port 4300");
+});
 // server listening on port 4300
-app.listen(4300);
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+/* cors means cross origin resource sharing it allows a frontend application such as react to connect with out backend */
+app.use(cors);
